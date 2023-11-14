dff85e9105c5ef2d3048a3f5f28a7d95015ddc32
bperezpinto <bperezpinto@argenprom.com>
2023-11-13 23:41:16 -0300

    comprobante de reserva

---
+++ app/Jobs/EnviarPagoReserva.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\ComprobantePagoReserva;
+use App\ReservaPropiedad;
+class EnviarPagoReserva implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+    protected $idPago;
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct($idPago)
+    {
+        $this->idPago = $idPago;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        try{
+            $estadosDePago = ReservaPropiedad::select('reservas_propiedades.*', 'pagos.montoPago', 'pagos.numeroTransaccion', 'pagos.secuenciaTransaccion',
+            'pagos.idMetodoPago', 'pagos.tokenPago', 'pagos.metodoPagoOtrosPagos', 'pagos.tipoPago', 'pagos.idPago')
+            ->join('pagos', 'pagos.tokenReserva', '=', 'reservas_propiedades.token')
+            ->leftjoin('metodos_pagos', 'metodos_pagos.idMetodosPagos', '=', 'pagos.idMetodoPago')
+            ->where('pagos.idPago', '=', $this->idPago)
+            ->first();
+            //return dd($this->estadosDePago);
+            $pdf = \PDF::loadView('emails.adjuntoPagoReservaArrendatario', [ 'estadosDePago' => $estadosDePago]);
+
+            //Mail::to($estadosDePago->email)
+            Mail::to(['beenjaahp@hotmail.com', 'beenjaahp@gmail.com'])
+            ->send(new ComprobantePagoReserva($estadosDePago, $pdf));
+        }
+        catch(\Exception $e)
+        {
+            Log::info($e->getMessage());
+        }
+    }
+}
