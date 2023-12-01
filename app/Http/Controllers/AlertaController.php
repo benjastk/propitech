e8bcd0b2f135d2a16d8756bc5716415bb68b6192
bperezpinto <bperezpinto@argenprom.com>
2023-12-01 15:31:29 -0300

    pruebas

--- app/Http/Controllers/AlertaController.php
+++ app/Http/Controllers/AlertaController.php
@@ -39,4 +39,34 @@ class AlertaController extends Controller
             }
         }
     }
+    public function pruebaMail()
+    {
+        $fechaActual = date('Y-m-d');
+        $diasAlerta1 = ParametroGeneral::where('parametroGeneral', '=', "ALERTA YA SE ENCUENTRA TU PAGO")->first();
+
+        $estadosPagos = EstadoPago::select('estados_pagos.*', 'users.email', 'users.id as idUsuario', 'users.name', 'users.apellido')
+        ->join('contratos_arriendos', 'contratos_arriendos.idContratoArriendo', '=', 'estados_pagos.idContrato')
+        ->join('users', 'users.id', '=', 'contratos_arriendos.idUsuarioArrendatario')
+        ->whereIn('estados_pagos.idEstado', [47])
+        ->get();
+        if(!$estadosPagos->isEmpty())
+        {
+            foreach ($estadosPagos as $estadoPago) 
+            { 
+                if($diasAlerta1->valorParametro != -1)
+                {
+                    //return date("Y-m-d",strtotime($estadoPago->fechaVencimiento."- ".$diasAlerta1->valorParametro." days"));
+                    if(date("Y-m-d",strtotime($estadoPago->fechaVencimiento."- ".$diasAlerta1->valorParametro." days")) == $fechaActual)
+                    {
+                        //return $estadoPago;
+                        YaSeEncuentraDisponibleTuPagoJob::dispatch($estadoPago);
+                        $nuevoLogCorreo = new LogCorreoEnviado();
+                        $nuevoLogCorreo->nombre_tipo_correo = 'RECORDATORIO PAGO DE ARRIENDO '. $diasAlerta1->valorParametro .' DIAS ANTES';
+                        $nuevoLogCorreo->usuario = 'CRON AUTOMATIZADO';
+                        $nuevoLogCorreo->save();
+                    }
+                }
+            }
+        }
+    }
 }
