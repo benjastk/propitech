63525d452a1c8e5c5df655b67c67ddd63dcf2431
bperezpinto <bperezpinto@argenprom.com>
2024-01-05 19:08:55 -0300

    csfaf

--- resources/views/back-office/home.blade.php
+++ resources/views/back-office/home.blade.php
@@ -10,10 +10,10 @@
 
                             <div class="page-title-right">
                                 <ol class="breadcrumb m-0">
-                                    <li class="breadcrumb-item"><a href="javascript: void(0);">Dashboard</a></li>
+                                    <li class="breadcrumb-item"><a href="javascript: void(0);">Dashboard - {{ $hora }}</a></li>
                                 </ol>
                             </div>
-
+                            
                         </div>
                     </div>
                 </div>
