01ead81894a0f3dd1b690ec642a3be1617e90a78
bperezpinto <bperezpinto@argenprom.com>
2024-03-04 15:04:20 -0300

    foreach

--- app/Http/Controllers/AlertaController.php
+++ app/Http/Controllers/AlertaController.php
@@ -131,41 +131,31 @@ class AlertaController extends Controller
     }
     public function recordarPagoSMS()
     {
-        /*$fechaActual = date('Y-m-d');
-        $anioActual = date('Y');
-        $mesActual = date('m');
-        $diasAlerta1 = ParametroGeneral::where('parametroGeneral', '=', "ALERTA YA SE ENCUENTRA TU PAGO")->first();
+        try{
+            $fechaActual = date('Y-m-d');
+            $anioActual = date('Y');
+            $mesActual = date('m');
+            $diasAlerta1 = ParametroGeneral::where('parametroGeneral', '=', "ALERTA YA SE ENCUENTRA TU PAGO")->first();
 
-        $estadosPagos = EstadoPago::select('estados_pagos.*', 'users.email', 'users.id as idUsuario', 'users.name', 'users.apellido', 'users.telefono')
-        ->join('contratos_arriendos', 'contratos_arriendos.idContratoArriendo', '=', 'estados_pagos.idContrato')
-        ->join('users', 'users.id', '=', 'contratos_arriendos.idUsuarioArrendatario')
-        ->whereIn('estados_pagos.idEstado', [47])
-        ->where('contratos_arriendos.idEstado', 61)
-        ->whereMonth('estados_pagos.fechaVencimiento', '=', $mesActual)
-        ->whereYear('estados_pagos.fechaVencimiento', '=', $anioActual)
-        ->get();
-        if(!$estadosPagos->isEmpty())
-        {
-            foreach ($estadosPagos as $estadoPago) 
-            { 
-                if($diasAlerta1->valorParametro != -1)
-                {
-                    if(date("Y-m-d",strtotime($estadoPago->fechaVencimiento."- ".$diasAlerta1->valorParametro." days")) == $fechaActual)
-                    {
-                        $enviar = SMS::sendSMS();
-                        $enviar['cliente']->messages->create( '+56'.$estadoPago->telefono,
-                                ['from' => $enviar['numero'], 'body' => 'Estimado arrendatario, ya se encuentra disponible el pago de tu arriendo del mes de Marzo.
-                                Puedes pagar en www.propitech.cl/pago-online. Equipo Propitech'] );
-                    }
+            $estadosPagos = EstadoPago::select('estados_pagos.*', 'users.email', 'users.id as idUsuario', 'users.name', 'users.apellido', 'users.telefono')
+            ->join('contratos_arriendos', 'contratos_arriendos.idContratoArriendo', '=', 'estados_pagos.idContrato')
+            ->join('users', 'users.id', '=', 'contratos_arriendos.idUsuarioArrendatario')
+            ->whereIn('estados_pagos.idEstado', [47])
+            ->where('contratos_arriendos.idEstado', 61)
+            ->whereMonth('estados_pagos.fechaVencimiento', '=', $mesActual)
+            ->whereYear('estados_pagos.fechaVencimiento', '=', $anioActual)
+            ->get();
+            if(!$estadosPagos->isEmpty())
+            {
+                foreach ($estadosPagos as $estadoPago) 
+                { 
+                    $enviar = SMS::sendSMS();
+                    $enviar['cliente']->messages->create( $estadoPago->telefono,
+                        ['from' => $enviar['numero'], 'body' => 'Estimado'. $estadoPago->name. ' '. $estadoPago->apellido.', ya se encuentra disponible el pago de tu arriendo del mes de Marzo.
+                        Puede pagar en www.propitech.cl/pago-online. Equipo Propitech'] );
                 }
             }
-        }*/
-        try {
-            $enviar = SMS::sendSMS();
-            $enviar['cliente']->messages->create( '+56989583599',
-                ['from' => $enviar['numero'], 'body' => 'Estimado arrendatario, ya se encuentra disponible el pago de tu arriendo del mes de Marzo.
-                Puedes pagar en www.propitech.cl/pago-online. Equipo Propitech'] );
-            return response()->json($enviar);
+            return "listo";
         } catch (\Exception $e) {
             return response()->json($e->getMessage());
         }
