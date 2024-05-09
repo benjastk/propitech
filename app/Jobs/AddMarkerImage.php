80b56c8ceea1c3c187391a59c3cd7c2f6993fbc9
bperezpinto <bperezpinto@argenprom.com>
2024-05-08 20:54:34 -0400

    job

---
+++ app/Jobs/AddMarkerImage.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\File;
+use Intervention\Image\ImageManager;
+use App\Foto;
+use Image;
+class AddMarkerImage implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $fotos = Foto::where('marcaDeAgua', 0)->get();
+        if($fotos)
+        {
+            foreach ($fotos as $foto) 
+            {
+                $antiguo = $foto->nombreArchivo;
+                if(file_exists(('img/propiedad/' . $foto->nombreArchivo))) 
+                {
+                    $img = \Image::make(public_path('img/propiedad/' . $foto->nombreArchivo));
+                    /* insert watermark at bottom-right corner with 10px offset */
+                    $img->insert(public_path('front/logoopacity2.png'), 'center');
+                    $path = public_path() . '/img/propiedad/';
+        
+                    $fileName = uniqid().'.png';
+                    $img->save($path . $fileName);
+                    $foto->nombreArchivo = $fileName;
+                    $foto->marcaDeAgua = 1;
+                    $foto->save();
+                    File::delete(public_path('img/propiedad/' . $antiguo));
+                }
+            }
+        }
+    }
+}
