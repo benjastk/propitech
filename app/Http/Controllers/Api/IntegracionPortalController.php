6d56d1b915b3f2c33894b07b52eb7691481ba4ce
bperezpinto <bperezpinto@argenprom.com>
2024-10-09 21:34:16 -0300

    inicio sesion portal inmobiliario 2

--- app/Http/Controllers/Api/IntegracionPortalController.php
+++ app/Http/Controllers/Api/IntegracionPortalController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\User;
+use Auth;
+use Log;
 
 class IntegracionPortalController extends Controller
 {
